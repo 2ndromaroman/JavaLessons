b30b79eba270103a835b2e85b97803fbfe891327
RSidorov <r.sidorov.office@gmail.com>
2023-07-23 23:18:34 +0300

    lesson 2. Data types

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
 public class Main {
+
+//    Задание 1
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+//    Задание 2
+
+    double first = 1.3;
+    byte second = 2;
+
 }
