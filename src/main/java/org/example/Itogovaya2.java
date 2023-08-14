3d0125c3f61079d2dfe8b0835f53c47db3d872f1
RSidorov <r.sidorov.office@gmail.com>
2023-08-14 17:48:22 +0300

    lesson 8.

--- src/main/java/org/example/Itogovaya2.java
+++ src/main/java/org/example/Itogovaya2.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 
+// Сначала я разбиваю водную строку на левую и правую части уравнения, далее работаю с тими частями.
+// Здесь много багов и недочётов. Например, в случае если указать уравнение вида 1-10=x  в ответе x = 0,
+// т.к. вычисление идет по индексу
 public class Itogovaya2 {
 
     public static void main(String[] args) {
@@ -14,23 +17,59 @@ public class Itogovaya2 {
         if (vir.length() != 5) {
             System.out.println("Неверный формат");
         } else {
-            String[] split = vir.split("=");
-            String left = Arrays.toString(split[0].toCharArray());
-            String right = Arrays.toString(split[1].toCharArray());
-            if (String.valueOf(right.charAt(1)).equals("x")) {
-                if (String.valueOf(left.charAt(4)).equals("+")) {
-                    System.out.println("x = " + (Integer.parseInt(String.valueOf(left.charAt(1))) + Integer.parseInt(String.valueOf(left.charAt(7)))));
-                } else if (String.valueOf(left.charAt(4)).equals("-")) {
-                    System.out.println("x = " + (Integer.parseInt(String.valueOf(left.charAt(1))) - Integer.parseInt(String.valueOf(left.charAt(7)))));
+            char[] vir1 = vir.toCharArray();
+            String index1 = String.valueOf(vir1[0]);
+            String index2 = String.valueOf(vir1[2]);
+            String index3 = String.valueOf(vir1[4]);
+            String operation = String.valueOf(vir1[1]);
+            if (index1.equals("x")) {
+                switch (operation) {
+                    case ("+"):
+                        int a = Integer.parseInt(index3);
+                        int b = Integer.parseInt(index2);
+                        System.out.println("x = " + (b - a));
+                        break;
+                    case ("-"):
+                        int aa = Integer.parseInt(index3);
+                        int bb = Integer.parseInt(index2);
+                        System.out.println("x = " + (bb + aa));
+                        break;
+                    default:
+                        System.out.println("Error1");
                 }
+            } else if (index2.equals("x")) {
+                switch (operation) {
+                    case ("+"):
+                        int a = Integer.parseInt(index1);
+                        int b = Integer.parseInt(index3);
+                        System.out.println("x = " + (b - a));
+                        break;
+                    case ("-"):
+                        int aa = Integer.parseInt(index1);
+                        int bb = Integer.parseInt(index3);
+                        System.out.println("x = " + (bb + aa));
+                        break;
+                    default:
+                        System.out.println("Error2");
 
-            } else if (String.valueOf(left.charAt(1)).equals("x")) {
-                if (String.valueOf(right.charAt(4)).equals("+")) {
-                    System.out.println("x = " + (Integer.parseInt(String.valueOf(right.charAt(1))) + Integer.parseInt(String.valueOf(right.charAt(7)))));
-                } else if (String.valueOf(right.charAt(4)).equals("-")) {
-                    System.out.println("x = " + (Integer.parseInt(String.valueOf(right.charAt(1))) - Integer.parseInt(String.valueOf(right.charAt(7)))));
                 }
-            } else System.out.println("Не корректный ввод");
+            } else if (index3.equals("x")) {
+                switch (operation) {
+                    case ("+"):
+                        int a = Integer.parseInt(index1);
+                        int b = Integer.parseInt(index2);
+                        System.out.println("x = " + (a + b));
+                        break;
+                    case ("-"):
+                        int aa = Integer.parseInt(index1);
+                        int bb = Integer.parseInt(index2);
+                        System.out.println("x = " + (aa - bb));
+                        break;
+                    default:
+                        System.out.println("Error3");
+                }
+            }
+
         }
     }
 }
